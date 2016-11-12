19a275a9e55dd2ab2edd2c3efde059781189389f
bsteinnyc <bsteinnyc@gmail.com>
2016-11-12 01:40:07 -0500

    Update history.js

--- history.js
+++ history.js
@@ -1,10 +1,12 @@
 document.getElementById("summarySheet").addEventListener("click", function () {
     summarySheet();
 });
+document.getElementById("clear").addEventListener("click", function () {
+    clearStore();
+});
 document.getElementById("submit").addEventListener("click", function () {
     readResults();
 });
-
 function readResults() {
     var stuff = document.getElementById('contents')
         , stuffContents = stuff.getElementsByTagName('*');
@@ -20,17 +22,27 @@ function readResults() {
 
 function summarySheet() {
     document.getElementById("summarySheet").parentNode.removeChild(document.getElementById("summarySheet"));
-    document.getElementById("title").innerHTML = document.getElementById("title").innerHTML + " Summary";
-    for (var i = 0;i < localStorage.length; ++i) {
+    for (var i = 0;i < localStorage.length; i++) {
+        console.log(localStorage.key(i));
         if (document.getElementById(localStorage.key(i)).type == "checkbox") {
-            document.getElementById(localStorage.key(i)).checked = (localStorage.getItem(localStorage.key(i)) == true);
+            if(localStorage.getItem(localStorage.key(i)).checked){
+            document.getElementById(localStorage.key(i)).checked = (localStorage.getItem(localStorage.key(i)) == "on");
+        }
+        else{
+            document.getElementById(localStorage.key(i)).checked = false;
+        }
+            document.getElementById(localStorage.key(i)).disabled = "true";
         }
         if (document.getElementById(localStorage.key(i)).type == "text") {
             document.getElementById(localStorage.key(i)).value = localStorage.getItem(localStorage.key(i));
+            document.getElementById(localStorage.key(i)).disabled = "true";
         }
-            document.getElementById(localStorage.key(i)).disabled = true;
+            
     }
 
+}
+function clearStore(){
+    localStorage.clear();
 }
 var jsontext = JSON.parse('{ "menstrual history": { "title": "Menstrual History", "Questions": [{ "question": "Day of last period(first day):", "answers": [{ "type": "textbox" }] }, { "question": "Are you menopausal?", "answers": [{ "text": "yes", "type": "checkbox" }, { "text": "no", "type": "checkbox" }] }, { "question": "Do you take oral contraceptives?", "answers": [{ "text": "yes", "type": "checkbox" }, { "text": "no", "type": "checkbox" }] }, { "question": "If yes, please list the date you started:", "answers": [{ "type": "textbox" }] }, { "question": "PRE-INJURY:What is your average length of cycle (1st day to 1st day)?", "answers": [{ "type": "textbox" }] }, { "question": "Have you missed any periods?", "answers": [{ "text": "yes", "type": "checkbox" }, { "text": "no", "type": "checkbox" }] }, { "question": "If yes, how many and when?", "answers": [{ "type": "textbox" }] }, { "question": "Describe your periods", "answers": [{ "text": "Light", "type": "checkbox" }, { "text": "Moderate", "type": "checkbox" }, { "text": "Heavy", "type": "checkbox" }] }, { "question": "POST-INJURY:What is your average length of cycle (1st day to 1st day)?", "answers": [{ "type": "textbox" }] }, { "question": "Have you missed any periods?", "answers": [{ "text": "yes", "type": "checkbox" }, { "text": "no", "type": "checkbox" }] }, { "question": "If yes, how many and when?", "answers": [{ "type": "textbox" }] }, { "question": "Describe your periods", "answers": [{ "text": "Light", "type": "checkbox" }, { "text": "Moderate", "type": "checkbox" }, { "text": "Heavy", "type": "checkbox" }] }] } }');
 var title = document.createElement('e1');
@@ -57,7 +69,7 @@ for (i = 0; i < jsontext["menstrual history"]["Questions"].length; i++) {
         }
         if (jsontext["menstrual history"]["Questions"][i].answers[h].type.indexOf("checkbox") != -1) {
             input.type = "checkbox";
-            input.id = "question: " + i + " answer: " + h + " check";
+            input.id = "question: " + i + " answer: " + h + "chk";
         }
         if (input.id != "") {
             contents1.appendChild(input);
